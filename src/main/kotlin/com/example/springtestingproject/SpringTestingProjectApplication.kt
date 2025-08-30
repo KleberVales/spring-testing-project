d79b45e067e7a55d23180ab7f8ac5ec6acf56109
Kleber Vales <klebervales.dev@gmail.com>
2025-08-30 13:37:35 -0300

    Setup initial

---
+++ src/main/kotlin/com/example/springtestingproject/SpringTestingProjectApplication.kt
@@ -0,0 +1,11 @@
+package com.example.springtestingproject
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringTestingProjectApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringTestingProjectApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/springtestingproject/SpringTestingProjectApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.springtestingproject
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringTestingProjectApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
